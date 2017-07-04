c940e16af1f24240245c56fdabb5d8918a61671e
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-04 21:26:02 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,11 @@
+# set1
+import java.util.*;
+import java.io.*;
+public class StringToInt{  
+  public static void main(String args[]){  
+    Scanner sc=new Scanner(System.in); 
+    String s=sc.nextLine();  
+    int i=Integer.parseInt(s);  
+    System.out.println(s+100); 
+    System.out.println(i+100); 
+  }} 
